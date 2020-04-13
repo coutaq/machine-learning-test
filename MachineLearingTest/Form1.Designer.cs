266d133fbe833edffc5a72bf9eb0d016d7aeacd0
Michael Melikov <coutaq@gmail.com>
2020-04-13 23:44:33 +0300

    Add project files.

---
+++ MachineLearingTest/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MachineLearningTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonWhite = new System.Windows.Forms.Button();
+            this.buttonBlack = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonWhite, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonBlack, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 93);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 354);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // buttonWhite
+            // 
+            this.buttonWhite.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonWhite.FlatAppearance.BorderSize = 0;
+            this.buttonWhite.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWhite.Font = new System.Drawing.Font("Gotham Medium", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWhite.ForeColor = System.Drawing.Color.White;
+            this.buttonWhite.Location = new System.Drawing.Point(400, 3);
+            this.buttonWhite.Name = "buttonWhite";
+            this.buttonWhite.Size = new System.Drawing.Size(391, 348);
+            this.buttonWhite.TabIndex = 0;
+            this.buttonWhite.Text = "TEXT";
+            this.buttonWhite.UseVisualStyleBackColor = true;
+            this.buttonWhite.Click += new System.EventHandler(this.buttonWhite_Click);
+            // 
+            // buttonBlack
+            // 
+            this.buttonBlack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonBlack.FlatAppearance.BorderSize = 0;
+            this.buttonBlack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBlack.Font = new System.Drawing.Font("Gotham Medium", 71.99999F);
+            this.buttonBlack.ForeColor = System.Drawing.Color.Black;
+            this.buttonBlack.Location = new System.Drawing.Point(3, 3);
+            this.buttonBlack.Name = "buttonBlack";
+            this.buttonBlack.Size = new System.Drawing.Size(391, 348);
+            this.buttonBlack.TabIndex = 1;
+            this.buttonBlack.Text = "TEXT";
+            this.buttonBlack.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.buttonBlack.UseVisualStyleBackColor = false;
+            this.buttonBlack.Click += new System.EventHandler(this.buttonBlack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonWhite;
+        private System.Windows.Forms.Button buttonBlack;
+    }
+}
+
---
+++ MachineLearingTest/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineLearningTest
+{
+    public partial class Form1 : Form
+    {
+        List<HSBColor> blacks = new List<HSBColor>();
+        List<HSBColor> whites = new List<HSBColor>();
+        public Form1()
+        {
+            InitializeComponent();
+            newColor();
+        }
+
+        private void buttonBlack_Click(object sender, EventArgs e)
+        {
+            Color buttonColor = buttonBlack.BackColor;
+            blacks.Add(new HSBColor(buttonColor.GetHue(), buttonColor.GetSaturation(), buttonColor.GetBrightness()));
+            newColor();
+        }
+
+        private void buttonWhite_Click(object sender, EventArgs e)
+        {
+            Color buttonColor = buttonWhite.BackColor;
+            whites.Add(new HSBColor(buttonColor.GetHue(), buttonColor.GetSaturation(), buttonColor.GetBrightness()));
+            newColor();
+        }
+
+        private void newColor()
+        {
+            Random r = new Random();
+            Color newColor = Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256));
+            buttonWhite.BackColor = newColor;
+            buttonWhite.FlatAppearance.MouseOverBackColor = newColor;
+            buttonBlack.BackColor = newColor;
+            buttonBlack.FlatAppearance.MouseOverBackColor = newColor;
+        }
+
+    }
+}
